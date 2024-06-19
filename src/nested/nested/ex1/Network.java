4cecf35c3f87f805170dedbe1d512fe179f9cb37
Jisu Lee <jisulee.shsf@gmail.com>
2024-06-19 20:30:03 +0900

    정적 중첩 클래스의 활용

---
+++ src/nested/nested/ex1/Network.java
@@ -0,0 +1,9 @@
+package nested.nested.ex1;
+
+public class Network {
+
+    public void sendMessage(String message) {
+        NetworkMessage networkMessage = new NetworkMessage(message);
+        networkMessage.print();
+    }
+}
---
+++ src/nested/nested/ex2/Network.java
@@ -0,0 +1,21 @@
+package nested.nested.ex2;
+
+public class Network {
+
+    public void sendMessage(String message) {
+        NetworkMessage networkMessage = new NetworkMessage(message);
+        networkMessage.print();
+    }
+
+    private static class NetworkMessage {
+        private String content;
+
+        public NetworkMessage(String content) {
+            this.content = content;
+        }
+
+        public void print() {
+            System.out.println(content);
+        }
+    }
+}
