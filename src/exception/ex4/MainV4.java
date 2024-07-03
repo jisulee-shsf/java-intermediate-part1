878c489f3490e103b2f3411dc54de1dd63a252b0
Jisu Lee <jisulee.shsf@gmail.com>
2024-07-03 23:58:32 +0900

    try-with-resources

--- src/exception/ex4/MainV4.java
+++ src/exception/ex4/MainV4.java
@@ -1,11 +1,14 @@
 package exception.ex4;
 
+import exception.ex4.exception.SendExceptionV4;
+
 import java.util.Scanner;
 
 public class MainV4 {
 
     public static void main(String[] args) {
-        NetworkServiceV4 networkService = new NetworkServiceV4();
+//        NetworkServiceV4 networkService = new NetworkServiceV4();
+        NetworkServiceV5 networkService = new NetworkServiceV5();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -14,9 +17,26 @@ public class MainV4 {
             if (input.equals("exit")) {
                 break;
             }
-            networkService.sendMessage(input);
+            try {
+                networkService.sendMessage(input);
+            } catch (Exception e) {
+                exceptionHandler(e);
+            }
             System.out.println();
         }
         System.out.println("프로그램을 정상 종료합니다.");
+    }
+
+    // 공통 예외 처리
+    private static void exceptionHandler(Exception e) {
+        System.out.println("죄송합니다. 알 수 없는 오류가 발생했습니다.");
+        System.out.println("=====개발자용 디버깅 메시지=====");
+        e.printStackTrace(System.out);
+//        e.printStackTrace();
+
+        // 별도 예외 처리
+        if (e instanceof SendExceptionV4 sendEx) {
+            System.out.println("[전송 오류] 전송 데이터: " + sendEx.getSendData());
+        }
     }
 }
